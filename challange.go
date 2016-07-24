952a9bcbd03ea4d05ec3eb1a5514ada39edc7f05
w1n2k <cryfall@gmail.com>
2016-07-24 23:23:22 +0300

    Main code of challange

---
+++ challange.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/arbovm/levenshtein"
+)
+
+// Reading vocabulary file
+func main() {
+	vocabulary := loadVocabulary("./vocabulary.txt")
+	words := loadWords("./187")
+
+	count := len(words)
+	c := make(chan int, 30)
+	for i := range words {
+		go func(word string) {
+			length := len(word)
+			wordDistance := length
+
+			// Trying to find distance in main vocab
+			if slice, ok := vocabulary[length]; ok {
+				wordDistance = searchInSlice(slice, word)
+			}
+
+			// Now we will try to find distance
+			shoulder := 1
+			if wordDistance >= shoulder {
+				for shoulder < wordDistance {
+					leftDistance := wordDistance
+					rightDistance := wordDistance
+
+					if leftSlice, ok := vocabulary[length-shoulder]; ok {
+						leftDistance = searchInSlice(leftSlice, word)
+					}
+
+					if leftDistance >= wordDistance {
+						if rightSlice, ok := vocabulary[length+shoulder]; ok {
+							rightDistance = searchInSlice(rightSlice, word)
+						}
+
+						if rightDistance < wordDistance {
+							wordDistance = rightDistance
+						}
+					} else {
+						wordDistance = leftDistance
+					}
+
+					shoulder++
+				}
+			}
+
+			c <- wordDistance
+		}(words[i])
+	}
+
+	totalDistane := 0
+	for i := 1; i <= count; i++ {
+		totalDistane = totalDistane + <-c
+	}
+
+	fmt.Println(totalDistane)
+}
+
+// Load vocabulary file
+func loadVocabulary(filename string) map[int][]string {
+	vocabulary := make(map[int][]string)
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		word := strings.ToUpper(scanner.Text())
+		wordLength := len(word)
+
+		vocabulary[wordLength] = append(vocabulary[wordLength], word)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return vocabulary
+}
+
+// Load words file
+func loadWords(filename string) []string {
+	var words []string
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+
+		for i := range parts {
+			words = append(words, strings.ToUpper(parts[i]))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return words
+}
+
+// Searching in slice
+func searchInSlice(slice []string, word string) int {
+	wordDistance := len(word)
+
+	for j := range slice {
+		tmp := levenshtein.Distance(word, slice[j])
+
+		if tmp < wordDistance {
+			wordDistance = tmp
+		}
+
+		if wordDistance == 0 {
+			break
+		}
+	}
+
+	return wordDistance
+}
